b4733e092312eb2d766a0f1cf12f1291d2ec3026
depers <dev_fengxiao@163.com>
2020-01-15 21:39:22 +0800

    5.4 购物车商品删除

--- mall-api/src/main/java/cn/bravedawn/controller/ShopcatController.java
+++ mall-api/src/main/java/cn/bravedawn/controller/ShopcatController.java
@@ -40,4 +40,23 @@ public class ShopcatController {
 
         return JsonResult.ok();
     }
+
+
+    @ApiOperation(value = "从购物车中删除商品", notes = "从购物车中删除商品", httpMethod = "POST")
+    @PostMapping("/del")
+    public JsonResult del(
+            @RequestParam String userId,
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
+            return JsonResult.errorMsg("参数不能为空");
+        }
+
+        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
+
+        return JsonResult.ok();
+    }
 }
