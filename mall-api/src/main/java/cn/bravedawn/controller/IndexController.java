18070c02b373833686988e87457deb6ff19b50e6
depers <dev_fengxiao@163.com>
2020-04-19 18:53:42 +0800

    2-5 基于Redis优化首页轮播图查询

--- mall-api/src/main/java/cn/bravedawn/controller/IndexController.java
+++ mall-api/src/main/java/cn/bravedawn/controller/IndexController.java
@@ -6,11 +6,15 @@ import cn.bravedawn.pojo.Category;
 import cn.bravedawn.service.CarouselService;
 import cn.bravedawn.service.CategoryService;
 import cn.bravedawn.utils.JsonResult;
+import cn.bravedawn.utils.JsonUtils;
+import cn.bravedawn.utils.RedisOperator;
 import cn.bravedawn.vo.CategoryVO;
 import cn.bravedawn.vo.NewItemsVO;
+import com.google.common.collect.Lists;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,12 +39,31 @@ public class IndexController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private RedisOperator redisOperator;
+
     @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
     @GetMapping("/carousel")
     public JsonResult carousel() {
-        List<Carousel> list = carouselService.queryAll(YesOrNo.YES.type);
+
+        List<Carousel> list = Lists.newArrayList();
+        String carouselStr = redisOperator.get("carousel");
+
+        if (StringUtils.isBlank(carouselStr)) {
+            list = carouselService.queryAll(YesOrNo.YES.type);
+            redisOperator.set("carousel", JsonUtils.objectToJson(list));
+        } else {
+            list = JsonUtils.jsonToList(carouselStr, Carousel.class);
+        }
         return JsonResult.ok(list);
     }
+
+    /**
+     * 如果广告（轮播图）发生更改应该怎么做？
+     * 1. 后台运营系统，一旦广告（轮播图）发生更改，就可以删除缓存，然后重置
+     * 2. 定时重置，比如每天凌晨三点重置
+     * 3. 每个轮播图都有可能是一个广告，每个广告都会有一个过期时间，过期了再重置
+     */
 
 
     /**
