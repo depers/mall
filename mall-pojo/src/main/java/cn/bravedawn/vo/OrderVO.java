6ea32e67dc73abf5673ddcfb000c41c0bf82e66b
depers <dev_fengxiao@163.com>
2020-04-21 21:17:07 +0800

    2-8 Redis 购物车 - 清理已结算商品

--- mall-pojo/src/main/java/cn/bravedawn/vo/OrderVO.java
+++ mall-pojo/src/main/java/cn/bravedawn/vo/OrderVO.java
@@ -1,10 +1,16 @@
 package cn.bravedawn.vo;
 
+import cn.bravedawn.bo.ShopcartBO;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class OrderVO {
 
     private String orderId;
+
     private MerchantOrdersVO merchantOrdersVO;
+
+    private List<ShopcartBO> toBeRemovedShopcartList;
 }
