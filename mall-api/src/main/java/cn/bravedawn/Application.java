56d46b44cccc42424c6ba1817fbf12eda86a8799
depers <dev_fengxiao@163.com>
2020-07-12 17:10:22 +0800

    第1章 Redis实现用户会话完结

--- mall-api/src/main/java/cn/bravedawn/Application.java
+++ mall-api/src/main/java/cn/bravedawn/Application.java
@@ -2,6 +2,7 @@ package cn.bravedawn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -12,7 +13,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @Author 冯晓
  * @Date 2019/12/26 20:12
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 扫描mybatis通用mapper所在的包
 @MapperScan("cn.bravedawn.mapper")
 // 扫描所有包以及相关组件包
