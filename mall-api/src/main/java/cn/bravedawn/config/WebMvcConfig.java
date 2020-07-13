afc3e4318cf6de1396047953088820c47a0165e2
depers <dev_fengxiao@163.com>
2020-07-13 22:41:13 +0800

    分布式会话拦截器

--- mall-api/src/main/java/cn/bravedawn/config/WebMvcConfig.java
+++ mall-api/src/main/java/cn/bravedawn/config/WebMvcConfig.java
@@ -1,11 +1,13 @@
 package cn.bravedawn.config;
 
+import cn.bravedawn.controller.interceptor.UserTokenInterceptor;
 import cn.bravedawn.resource.FileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,6 +27,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return builder.build();
     }
 
+    @Bean
+    public UserTokenInterceptor userTokenInterceptor(){
+        return new UserTokenInterceptor();
+    }
+
     // 实现静态资源的映射
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -34,5 +41,30 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/")
                 // 映射本地资源路径，该路径下方文件就都可以访问，切记配置路径最后的/
                 .addResourceLocations("file:" + fileUpload.getResourceLocations());
+    }
+
+    /**
+     * 注册拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userTokenInterceptor())
+                .addPathPatterns("/hello")
+                .addPathPatterns("/shopcart/add")
+                .addPathPatterns("/shopcart/del")
+                .addPathPatterns("/address/list")
+                .addPathPatterns("/address/add")
+                .addPathPatterns("/address/update")
+                .addPathPatterns("/address/setDefault")
+                .addPathPatterns("/orders/*")
+                .addPathPatterns("/center/*")
+                .addPathPatterns("/userInfo/*")
+                .addPathPatterns("/myorders/*")
+                .addPathPatterns("/mycomments/*")
+                .excludePathPatterns("/myorders/deliver")
+                .excludePathPatterns("/orders/notifyMerchantOrderPaid");
+
+        WebMvcConfigurer.super.addInterceptors(registry);
     }
 }
