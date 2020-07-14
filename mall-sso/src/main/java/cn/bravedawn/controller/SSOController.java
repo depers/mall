ae3ae39655d63ab4e6adecb89c3c957eb5c5bb1e
depers <dev_fengxiao@163.com>
2020-07-14 21:47:10 +0800

    CAS单点登录 - 跳转至登录

--- mall-sso/src/main/java/cn/bravedawn/controller/SSOController.java
+++ mall-sso/src/main/java/cn/bravedawn/controller/SSOController.java
@@ -1,7 +1,11 @@
 package cn.bravedawn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author 冯晓
@@ -11,9 +15,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SSOController {
 
 
-    @GetMapping("/hello")
-    public Object hello(){
-        return "hello world~";
+    @GetMapping("/login")
+    public String login(String returnUrl,
+                        Model model,
+                        HttpServletRequest request,
+                        HttpServletResponse response){
+
+        model.addAttribute("returnUrl", returnUrl);
+
+        // TODO 后续完善校验是否登录
+
+        // 用户从未登录过，第一次进入则跳转到CAS的统一登录页面
+        return "login";
     }
 
 }
