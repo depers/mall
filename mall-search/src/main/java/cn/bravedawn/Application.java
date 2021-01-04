029d0d968533728f9d5fc41a0aa8f92e1df88834
depers <dev_fengxiao@163.com>
2021-01-04 22:05:54 +0800

    6-1 elasticsearch整合springboot - 配置文件

---
+++ mall-search/src/main/java/cn/bravedawn/Application.java
@@ -0,0 +1,17 @@
+package cn.bravedawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+
+/**
+ * @Author 冯晓
+ * @Date 2019/12/26 20:12
+ */
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
