5d5252bb0d6b431c41dfb8ad6be2f7130c4f3157
depers <dev_fengxiao@163.com>
2021-01-14 21:03:02 +0800

    6-4 elasticsearch整合springboot-使用Java代码创建索引的弊端

--- mall-search/src/main/java/cn/bravedawn/es/pojo/Student.java
+++ mall-search/src/main/java/cn/bravedawn/es/pojo/Student.java
@@ -3,6 +3,7 @@ package cn.bravedawn.es.pojo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 /**
  * @author : depers
@@ -22,6 +23,15 @@ public class Student {
 
     @Field(store = true)
     private Integer age;
+
+    @Field(store = true)
+    private Float money;
+
+    @Field(store = true, type = FieldType.Keyword)
+    private String sign;
+
+    @Field(store = true)
+    private String description;
 
     public Long getId() {
         return id;
@@ -45,5 +55,29 @@ public class Student {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- mall-search/src/test/java/cn/bravedawn/ESTest.java
+++ mall-search/src/test/java/cn/bravedawn/ESTest.java
@@ -7,8 +7,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
@@ -27,21 +29,37 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ESTest {
 
     @Autowired
-    private ElasticsearchRestTemplate esTemplate;
+    private ElasticsearchOperations elasticsearchOperations;
 
     @Autowired
     ElasticsearchRestClientProperties properties;
 
+
+    /**
+     * 不建议使用 ElasticsearchTemplate 对索引进行管理（创建索引，更新索引，删除索引）
+     * 原因：1.索引就像是数据库或者数据库中的表，我们平时是不会通过Java代码频繁的去创建修改珊瑚数据库或者是表的；
+     *      我们只会针对数据做CRUD的操作，在es中也是同理，我们尽量使用ElasticsearchTemplate 对文档数据做CRUD的操作
+     *      2. 声明文档对象时(FieldTYpe)类型不灵活
+     *      3. 主分片与副本分片树无法设置
+     */
     @Test
     public void createIndexStudent(){
         Student student = new Student();
         student.setId(1L);
         student.setName("王明");
         student.setAge(18);
+        // 更新文档和数据
+        student.setMoney(18.8f);
+        student.setSign("i am spider man");
+        student.setDescription("I wish i am spider man");
 
         IndexQuery indexQuery = new IndexQueryBuilder().withObject(student).build();
-        esTemplate.index(indexQuery, IndexCoordinates.of("stu"));
+        elasticsearchOperations.index(indexQuery, IndexCoordinates.of("student"));
+    }
 
-        log.info(properties.getUris().toString());
+    @Test
+    public void deleteIndexStudent(){
+        IndexOperations indexOperations = elasticsearchOperations.indexOps(Student.class);
+        indexOperations.delete();
     }
 }
