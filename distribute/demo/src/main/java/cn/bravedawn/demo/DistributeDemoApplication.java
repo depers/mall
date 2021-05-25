b675d6dfb177a3bcc4ad96f1710df9b39411c40b
depers <dev_fengxiao@163.com>
2021-05-25 22:41:20 +0800

    2021-05-25提交

--- distribute/demo/src/main/java/cn/bravedawn/demo/DistributeDemoApplication.java
+++ distribute/demo/src/main/java/cn/bravedawn/demo/DistributeDemoApplication.java
@@ -3,9 +3,11 @@ package cn.bravedawn.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("cn.bravedawn.demo.dao")
+@EnableScheduling
 public class DistributeDemoApplication {
 
     public static void main(String[] args) {
--- distribute/demo/src/main/java/cn/bravedawn/demo/controller/RedisLockController.java
+++ distribute/demo/src/main/java/cn/bravedawn/demo/controller/RedisLockController.java
@@ -2,8 +2,17 @@ package cn.bravedawn.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author : depers
@@ -17,12 +26,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisLockController {
 
     @Autowired
-//    private RedisT
+    private RedisTemplate redisTemplate;
 
     @RequestMapping("redisLock")
-    public String redisLock() {
+    public String redisLock() throws InterruptedException {
         log.info("我进入了方法");
-        return "";
+
+        String key = "redisKey";
+        String value = UUID.randomUUID().toString();
+
+        RedisCallback<Boolean> redisCallback = connection -> {
+            // 设置NX
+            RedisStringCommands.SetOption setOption = RedisStringCommands.SetOption.ifAbsent();
+            // 设置时间
+            Expiration expiration = Expiration.seconds(30);
+            // 序列化key
+            byte[] redisKey = redisTemplate.getKeySerializer().serialize(key);
+            // 序列化value
+            byte[] redisValue = redisTemplate.getValueSerializer().serialize(value);
+            // 执行setNx操作
+            Boolean result = connection.set(redisKey, redisValue, expiration, setOption);
+            return result;
+        };
+
+        Boolean lock = (Boolean) redisTemplate.execute(redisCallback);
+
+        Thread.sleep(60000);
+
+        if (lock) {
+            String script = "if redis.call(\"get\", KEYS[1]) == ARGV[1] then\n" +
+                    "\treturn redis.call(\"del\", KEYS[1])\n" +
+                    "else\n" +
+                    "\treturn 0\n" +
+                    "end";
+            RedisScript<Boolean> redisScript = RedisScript.of(script, Boolean.class);
+            List<String> keys = Arrays.asList(key);
+
+            Boolean result = (Boolean) redisTemplate.execute(redisScript, keys, value);
+            log.info("释放锁的结果：" + result);
+        }
+
+        log.info("方法执行完成");
+        return "方法执行完成";
     }
 
 }
