f8cf8044648e0077e6fc28a4057760a99220317c
fengx9 <dev_fengxiao@163.com>
2022-03-10 18:31:53 +0800

    2022-03-10

---
+++ JavaTrain/src/main/java/cn/bravedawn/annotation/Color.java
@@ -0,0 +1,10 @@
+package cn.bravedawn.annotation;
+
+public enum Color {
+
+    BLUR,
+
+    RED,
+
+    GREEN;
+}
---
+++ JavaTrain/src/main/java/cn/bravedawn/annotation/FruitColor.java
@@ -0,0 +1,11 @@
+package cn.bravedawn.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FruitColor {
+
+    public enum Color{BLUE, RED, BLACK}
+}
