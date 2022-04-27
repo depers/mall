23516bd6ab0e4abe845ad7d4d8480e66ab3d717b
depers <dev_fengxiao@163.com>
2022-04-27 08:00:59 +0800

    2022-04-27 Writing dynamic content in Servlet response

---
+++ JavaTrain/src/main/java/cn/bravedawn/basic/operator/bit/AndExample.java
@@ -0,0 +1,28 @@
+package cn.bravedawn.basic.operator.bit;
+
+/**
+ * @author : depers
+ * @program : JavaTrain
+ * @date : Created in 2022/3/13 12:13
+ */
+public class AndExample {
+
+    /**
+     * 由下面的案例我们可知，奇数和1做与运算是等于1的，偶数与1做与运算是等于0的
+     * @param args
+     */
+
+
+    public static void main(String[] args) {
+        int a = 1; // 0001
+        int b = 2; // 0010
+        int c = 3; // 0011
+        int d = 4; // 0100
+
+        System.out.println(a & 1); // 1
+        System.out.println(b & 1); // 0
+        System.out.println(c & 1); // 1
+        System.out.println(d & 1); // 0
+
+    }
+}
