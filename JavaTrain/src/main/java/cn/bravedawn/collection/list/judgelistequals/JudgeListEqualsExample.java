e43ca6716f2dbfe496a22e284869edd978c13c99
depers <dev_fengxiao@163.com>
2022-09-28 08:00:36 +0800

    2022-09-28

--- JavaTrain/src/main/java/cn/bravedawn/collection/list/judgelistequals/JudgeListEqualsExample.java
+++ JavaTrain/src/main/java/cn/bravedawn/collection/list/judgelistequals/JudgeListEqualsExample.java
@@ -11,15 +11,18 @@ import java.util.List;
  */
 public class JudgeListEqualsExample {
 
+    /**
+     * 使用列表自身实现的equals方法就可以保证列表中按顺序每个元素是相同的
+     */
+
     public static void main(String[] args) {
         List<Integer> first = Arrays.asList(1, 3, 4, 6, 8);
         List<Integer> second = Arrays.asList(8, 1, 6, 3, 4);
         List<Integer> third = Arrays.asList(1, 3, 3, 6, 6);
         List<Integer> forth = Arrays.asList(1, 3, 4, 6, 8);
 
-        System.out.println(first.equals(forth));
-
-
+        System.out.println(first.equals(forth)); // true
+        System.out.println(first.equals(second)); // false
 
     }
 
---
+++ JavaTrain/src/main/java/cn/bravedawn/collection/list/judgelistequals/JudgeListEqualsExample2.java
@@ -0,0 +1,19 @@
+package cn.bravedawn.collection.list.judgelistequals;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * @author : depers
+ * @program : JavaTrain
+ * @date : Created in 2022/9/28 7:57
+ */
+public class JudgeListEqualsExample2 {
+
+
+    public static void main(String[] args) {
+        List<Integer> first = Lists.newArrayList(1, 2, 3, 4);
+        List<Integer> second = Lists.newArrayList(1, 2, 3, 4);
+    }
+}
