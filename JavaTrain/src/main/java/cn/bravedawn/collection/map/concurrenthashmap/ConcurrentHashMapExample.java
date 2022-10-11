59a2a3b70e1c4693fdcb11d960af473ae5c64210
depers <dev_fengxiao@163.com>
2022-10-11 22:14:20 +0800

    20221011

---
+++ JavaTrain/src/main/java/cn/bravedawn/collection/map/concurrenthashmap/ConcurrentHashMapExample.java
@@ -0,0 +1,15 @@
+package cn.bravedawn.collection.map.concurrenthashmap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author : depers
+ * @program : JavaTrain
+ * @date : Created in 2022/10/11 22:04
+ */
+public class ConcurrentHashMapExample {
+
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, Integer> concurrentHashMap = new ConcurrentHashMap<>();
+    }
+}
