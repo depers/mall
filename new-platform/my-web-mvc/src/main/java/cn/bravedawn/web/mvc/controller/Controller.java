7a2047d32eff7d8a0dc809f3f836862e1f0c9147
depers <dev_fengxiao@163.com>
2022-11-22 22:31:53 +0800

    2022-11-22

---
+++ new-platform/my-web-mvc/src/main/java/cn/bravedawn/web/mvc/controller/Controller.java
@@ -0,0 +1,10 @@
+package cn.bravedawn.web.mvc.controller;
+
+/**
+ * @author : depers
+ * @program : new-platform
+ * @description:
+ * @date : Created in 2022/11/22 22:28
+ */
+public interface Controller {
+}
---
+++ new-platform/user-web/src/main/java/cn/bravedawn/controller/RegisterController.java
@@ -0,0 +1,14 @@
+package cn.bravedawn.controller;
+
+import cn.bravedawn.web.mvc.controller.Controller;
+
+/**
+ * @author : depers
+ * @program : new-platform
+ * @description:
+ * @date : Created in 2022/11/22 22:29
+ */
+
+@Path
+public class RegisterController implements Controller {
+}
