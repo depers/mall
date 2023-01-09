3355403751134efaab787ec0b61d4815ff772948
depers <dev_fengxiao@163.com>
2023-01-09 22:28:13 +0800

    2023-01-09

---
+++ jvm/jvm-demo/src/main/java/cn/bravedawn/jvm/classloader/MyClassLoader.java
@@ -0,0 +1,53 @@
+package cn.bravedawn.jvm.classloader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * @author : depers
+ * @program : jvm-demo
+ * @description:
+ * @date : Created in 2023/1/9 22:17
+ */
+public class MyClassLoader extends ClassLoader{
+
+    /**
+     * 自定义ClassLoader
+     */
+
+    private String className;
+
+    public MyClassLoader(String className) {
+        this.className = className;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] data = this.loadClassData(name);
+        return this.defineClass(name, data, 0, data.length);
+    }
+
+
+    private byte[] loadClassData(String clsName) {
+        byte[] data = null;
+        InputStream in = null;
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        className = clsName.replace(".", "/");
+
+        try(out) {
+            in = new FileInputStream(new File("classes/" + className + ".class"));
+            int a = 0;
+            while ((a = in.read()) != -1) {
+                out.write(a);
+            }
+            data = out.toByteArray();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
+        return data;
+    }
+}
